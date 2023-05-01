e4c1e8114eb8fb16b922dd8ca64c82185d848112
hefecaso <hfcarrerasoto@yahoo.es>
2023-05-01 00:05:21 -0600

    checkpoint

--- Programación/FT2.py
+++ Programación/FT2.py
@@ -1,9 +1,33 @@
 import numpy as np
+import wave
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
 from scipy import signal
 
-# Cargar archivo de audio
+# Abrir archivo WAV
+with wave.open('filtromecanico.wav', 'r') as audio_file:
+    # Obtener los datos de audio
+    audio_data = audio_file.readframes(-1)
+    # Obtener la tasa de muestreo del audio
+    sample_rate = audio_file.getframerate()
+
+audio_data = np.frombuffer(audio_data, dtype=np.int16)
+window = np.hanning(len(audio_data))
+audio_data = audio_data * window
+
+# Calculamos la transformada de Fourier
+fourier = np.fft.fft(audio_data)
+
+# Obtenemos la magnitud y la fase de la transformada
+magnitud = np.abs(fourier)
+fase = np.angle(fourier)
+
+# Obtenemos la respuesta en frecuencia
+freq = np.fft.fftfreq(len(audio_data), 1/sample_rate)[:len(audio_data)//2]
+response = magnitud[:len(audio_data)//2] * np.exp(1j*fase[:len(audio_data)//2])
+transfer_function = np.abs(response)
+
+# Cargar archivo de audio para el analisis de la funcion de transferencia
 fs, audio = wavfile.read('filtromecanico.wav')
 
 # Aplicar transformada de Fourier
@@ -12,13 +36,13 @@ f, Pxx = signal.periodogram(audio, fs)
 # Obtener función de transferencia del filtro
 H = np.sqrt(Pxx)
 
-# Graficar respuesta en frecuencia del filtro
-plt.figure()
-plt.plot(f, H)
 
-plt.xlabel('Frecuencia (Hz)')
+
+plt.figure()
+plt.plot(freq, transfer_function)
+
+plt.xlabel('Frecuencia [Hz]')
 plt.ylabel('Magnitud')
-plt.grid()
 
 
 # Determinar tipo de filtro
